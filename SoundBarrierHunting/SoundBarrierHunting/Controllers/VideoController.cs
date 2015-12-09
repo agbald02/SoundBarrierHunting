1f7862922f81d060e7545920763624ecf5552a17
agbald02 <agbald02@gmail.com>
2015-12-08 19:48:09 -0500

    Added Video Database and made changes

---
+++ SoundBarrierHunting/SoundBarrierHunting/Controllers/VideoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoundBarrierHunting.Models;
+
+namespace SoundBarrierHunting.Controllers
+{
+    public class VideoController : Controller
+    {
+            //new Video{ 
+            //        VideoId = 1,
+            //        VideoLink = "https://www.youtube.com/embed/66ImvhrhuHc"},
+            //new Video{
+            //        VideoId = 2,
+            //        VideoLink = "https://www.youtube.com/embed/4gK8jmXqtdk"}
+
+
+        //public ActionResult Videos(int id = 1)
+        //{
+            //Video link = videoLinks.Where(v => v.VideoId == id).First();
+            //return View(link);
+        //}
+
+        public ActionResult Videos()
+        {
+            return View();
+        }
+
+        public ActionResult AllVideos()
+        {
+            return View();
+        }
+    }
+}
