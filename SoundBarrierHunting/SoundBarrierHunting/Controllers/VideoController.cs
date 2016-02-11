7c705dc136dbfb365c53dfe722ad132e2a65d68a
agbald02 <agbald02@gmail.com>
2016-02-10 19:43:37 -0500

    Added all Database pieces needed for videos

--- SoundBarrierHunting/SoundBarrierHunting/Controllers/VideoController.cs
+++ SoundBarrierHunting/SoundBarrierHunting/Controllers/VideoController.cs
@@ -1,11 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using SoundBarrierHunting.Domain.Concrete;
 using SoundBarrierHunting.Domain.Interfaces;
+using SoundBarrierHunting.Models.ViewModels;
+using SoundBarrierHunting.Models;
+using System.Collections.Generic;
+using System;
 
 namespace SoundBarrierHunting.Controllers
 {
     public class VideoController : Controller
     {
+        private EFDbContext db = new EFDbContext();
         private IVideoRepository repository;
+        public int pageSize = 8;
 
         public VideoController(IVideoRepository videoRepository)
         {
@@ -17,9 +27,50 @@ namespace SoundBarrierHunting.Controllers
             return View();
         }
 
-        public ViewResult AllVideos()
+        public ActionResult Search(string videoCategory)
         {
-            return View(repository.Videos);
+            var videoCatList = new List<string>();
+
+            var CatQuery = from c in db.Videos
+                           orderby c.Category
+                           select c.Category;
+
+            videoCatList.AddRange(CatQuery.Distinct());
+            ViewBag.videoCategories = new SelectList(db.Videos
+                            .OrderBy(c => c.Category)
+                            .Select(c => c.Category)
+                            .Distinct());
+
+            var videoList = from v in db.Videos
+                            select v;
+
+            if (!string.IsNullOrEmpty(videoCategory))
+            {
+                videoList = videoList.Where(x => x.Category == videoCategory);
+            }
+
+            return View(videoCategory);
+        }
+
+        public ViewResult AllVideos(string videoCategory, int page = 1)
+        {
+            var search = Search(videoCategory);
+
+            VideosListViewModel model = new VideosListViewModel
+            {
+                Videos = repository.Videos
+                .OrderBy(v => v.ID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Videos.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
